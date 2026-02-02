8bf09ba4ff137d69e5a7298c95bb4c6ab2041060
t4w4ntm <tawanpbtm1234@gmail.com>
2026-02-02 21:25:55 +0700

    fix 3 keed

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -76,6 +76,7 @@ onUnmounted(() => {
       <button 
         @click="$emit('toggle')"
         class="text-slate-300 hover:text-white p-1 rounded-md transition-all duration-300 z-50 shrink-0"
+        :class="collapsed ? '' : 'ml-auto'"
         :title="collapsed ? 'Expand Sidebar' : 'Collapse Sidebar'"
       >
          <svg class="w-8 h-8 font-bold" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
